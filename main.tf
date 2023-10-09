d64c59edcd53480ecbece18b17a4569df92a8d44
The-indigo <adepojuadeyemi11@gmail.com>
2023-10-09 09:56:53 -0400

    lambda file archive

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 resource "aws_s3_bucket" "resumeBucket" {
-  bucket = var.BUCKET_NAME
+  bucket = var.FE_BUCKET_NAME
+}
+
+resource "aws_s3_bucket" "lambdaCountBucket" {
+  bucket = var.BE_BUCKET_NAME
 }
 
 resource "aws_s3_bucket_policy" "allowCloudfrontAccess" {
@@ -25,3 +29,15 @@ data "aws_iam_policy_document" "allowCloudfrontAccess" {
   }
 }
 
+data "archive_file" "lambdaCountFunction" {
+    type = "zip"
+    source_dir = var.SOURCEPATH
+    output_path = "${path.module}/cloudresumebe.zip"
+}
+
+resource "aws_s3_object" "lambdaCountBucket" {
+bucket = aws_s3_bucket.lambdaCountBucket.id
+key= "cloudresumebe.zip"
+source = data.archive_file.lambdaCountFunction.output_path
+etag = filemd5(data.archive_file.lambdaCountFunction.output_path)
+}
--- vars.tf
+++ vars.tf
@@ -1,6 +1,12 @@
 variable "REGION" {
   default = "ca-central-1"
 }
-variable "BUCKET_NAME" {
+variable "FE_BUCKET_NAME" {
 default = "ajdeyemiresumee"   
+}
+variable "BE_BUCKET_NAME" {
+default = "ajdeyemibebucket"   
+}
+variable "SOURCEPATH" {
+  default = "/Users/indigo/Documents/aws/cloudresumebe"
 }
